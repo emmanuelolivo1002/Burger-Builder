106fd16e55236a72f3394ac6108020685cd81724
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-05-28 16:35:04 -0500

    Dynamic add ingredients message

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,11 +7,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
 
-  const transformedIngredients = Object.keys(props.ingredients).map(ingKey => {
+  let transformedIngredients = Object.keys(props.ingredients).map(ingKey => {
     return [...Array(props.ingredients[ingKey])].map( (_, i) => {
       return <BurgerIngredient key={ingKey + i} type={ingKey} />;
     });
-  });
+  }).reduce((arr, el) => {
+    return arr.concat(el);
+  }, []);
+
+  console.log(transformedIngredients);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
