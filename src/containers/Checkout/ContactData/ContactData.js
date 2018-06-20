911154bfc94bfe1e3b82ef9176111138db4a3c24
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-06-20 17:12:48 -0500

    Update Order submit

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
@@ -93,8 +94,6 @@ class ContactData extends Component {
     formIsValid: false
   }
 
-
-
   orderHandler = (event) => {
     event.preventDefault();
     this.setState({loading: true});
@@ -104,7 +103,7 @@ class ContactData extends Component {
     }
 
     const order = {
-      ingredients: this.props.ingredients,
+      ingredients: this.props.ings,
       totalPrice: this.props.price,
       orderData: formData
     }
@@ -183,7 +182,13 @@ class ContactData extends Component {
       </div>
     );
   }
-
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice
+  };
+}
+
+export default connect(mapStateToProps)(ContactData);
